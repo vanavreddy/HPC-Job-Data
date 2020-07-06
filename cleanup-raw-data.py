d190f2bd04fa5a5c838714eb54cf5fc4d5238803
vanavreddy <vana_reddy@rediffmail.com>
2020-07-06 07:31:02 -0400

    IU data matches VT data
    Cleanup done

--- cleanup-raw-data.py
+++ cleanup-raw-data.py
@@ -4,77 +4,74 @@ Spyder Editor
 
 This is a temporary script file.
 """
-import csv
-import re
+
 import os
 import pandas as pd 
-import numpy as np
+from sys import exit
 
 pd.options.mode.use_inf_as_na = True
 
-with open('data.csv', 'w') as jobs_info:
-    writer = csv.writer(jobs_info)
+def cleanupData(raw_data_path, cleaned_data_path):
     
-    
-    os.remove('data.csv')
-    f = open('data.csv', 'a+')
-
-    with open('/Users/vanareddy/Job-Data/file1.csv') as jobs:                                                                                          
-
-        job_reader = pd.read_csv(jobs, delimiter=';', na_values = 'NA').iloc[:, 3:]
+    dir_conts = os.listdir(raw_data_path)
+    for file in dir_conts:
         
-        for row in range(len(job_reader)):
-            #print(str(job_reader.loc[row][0]).split(' '))
-            cols = str(job_reader.loc[row][0]).split(' ')
-            print(cols)
-            if len(cols) <= 17 :
-                print(len(cols))
-                pass
-            else:
-                print("Long " + str(len(cols)))
-                f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(\
-                                                cols[2], cols[0], cols[1], cols[3], \
-                                                cols[23], cols[12], cols[19], cols[5], \
-                                                cols[6], cols[4], cols[7], cols[18], cols[14], \
-                                                cols[24], cols[9]))
-                
+        csv_filename = os.path.splitext(file)[0]
+        
+        file = raw_data_path + '/' + file
+        
+        #remove file if it exists
+        csv_filename = cleaned_data_path + '/' + csv_filename + '.csv'
+        #os.remove(filename)
+        #create new csv file
+        f = open(csv_filename, 'w+')
+        
+        #write header
+        f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(
+                            'jobnameMD5', 'userMD5', 'groupMD5', 'queue', 'resources_used_vmem',\
+                          'Resource_List_nodes', 'Exit_status', 'qtime', 'etime', 'ctime',	'start',\
+                          'end', 'Resource_List_walltime',	'resources_used_walltime', 'exec_host'))
+        f.write("\n")
+    
+        with open(file) as jobs:                                                                                          
+    
+            job_reader = pd.read_csv(jobs, delimiter=';', na_values = 'NA').iloc[:, 3:]
             
-                f.write("\n")
-            f.flush()
-            '''
-            f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(\
-                                                cols[2], cols[0], cols[1], cols[3], \
-                                                cols[23], cols[12], cols[19], cols[5], \
-                                                cols[6], cols[4], cols[7], cols[18], cols[14], \
-                                              cols[24], cols[9]))
-            '''
+            for row in range(len(job_reader)):
+                cols = str(job_reader.loc[row][0]).split(' ')
+                #print(cols)
+                if len(cols) <= 17 :
+                    #print(len(cols))
+                    pass
+                elif len(cols) == 25:
+                    #write data
+                    f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(\
+                                                    cols[2].split('=')[1], cols[0].split('=')[1], \
+                                                    cols[1].split('=')[1], cols[3].split('=')[1], \
+                                                    cols[23].split('=')[1], cols[12].split('=')[1], \
+                                                    cols[19].split('=')[1], cols[5].split('=')[1], \
+                                                    cols[6].split('=')[1], cols[4].split('=')[1], \
+                                                    cols[7].split('=')[1], cols[18].split('=')[1], \
+                                                    cols[14].split('=')[1], cols[24].split('=')[1], \
+                                                    cols[9].split('=')[1]))
+                    f.write("\n")
+                else:
+                    #write data
+                    f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(\
+                                                    cols[2].split('=')[1], cols[0].split('=')[1], \
+                                                    cols[1].split('=')[1], cols[3].split('=')[1], \
+                                                    cols[24].split('=')[1], cols[13].split('=')[1], \
+                                                    cols[20].split('=')[1], cols[5].split('=')[1], \
+                                                    cols[6].split('=')[1], cols[4].split('=')[1], \
+                                                    cols[7].split('=')[1], cols[19].split('=')[1], \
+                                                    cols[15].split('=')[1], cols[25].split('=')[1], \
+                                                    cols[9].split('=')[1]))
+                    f.write("\n")   
+                f.flush()
             
-        '''
-        for job in job_reader:
-            job_col3 = job[3].replace('Resource_List.', '')#.split(' ')
-            #row = str(job[0]) + " " + job[1] + " " + job[2] + ' '.join(job_col3)
-            row = str(job[0]) + " " + job[1] + " " + job[2] + ' ' + str(job_col3).strip('[]')
-            #print(row)
-            if not job_col3:
-                print("Empty Line")
-                f.write("\n")
-                pass
-            else:
-                #print(job_col3)
-                #jobnameMD5	userMD5	groupMD5	queue	resources_used_vmem	Resource_List_nodes	
-                #Exit_status	qtime	etime	ctime	start	end	Resource_List_walltime	
-                #resources_used_walltime	exec_host
-                #print(str(job_col3.split(' ')))
-                #cols = str(job[3])[0:-1]
-                cols = ' '.join(map(str, job_col3.split(' ')))
-                print(cols)
-                #f.write("{}, ".format(*cols[2]))
-                f.write("\n")
-                #f.write("{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}".format(\
-                #                                cols[2], cols[0], cols[1], cols[3], \
-                #                                cols[23], cols[12], cols[19], cols[5], \
-                #                                cols[6], cols[4], cols[7], cols[18], cols[14], \
-                #                               cols[24], cols[9]))
-                
-            print("----")
-         '''      
+if __name__ == "__main__":
+    raw_data_path = '/Users/vanareddy/Job-Data/IU/Mason-Oct-Dec-AcctFiles'
+    cleaned_data_path = '/Users/vanareddy/Job-Data/IU-Data'
+    
+    cleanupData(raw_data_path, cleaned_data_path)
+
